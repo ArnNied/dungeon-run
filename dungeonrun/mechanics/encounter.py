99fef36fd80b85530046645765422fc28c9363c5
Andrien Wiandyano <arnnied03@gmail.com>
2021-11-27 22:25:15 +0700

    feat: added class `Encounter`

---
+++ dungeonrun/mechanics/encounter.py
@@ -0,0 +1,20 @@
+from dungeonrun.actor.base import BaseActor
+
+
+class Encounter:
+    def __init__(self, player: BaseActor, enemy: BaseActor):
+        self.player = player
+        self.enemy = enemy
+
+        self.display_interface()
+
+    def display_interface(self) -> None:
+        print(
+            "========================================================================================================",
+            self.enemy.stringify_prop(),
+            "",
+            self.player.stringify_prop(),
+            "========================================================================================================",
+            sep="\n"
+            )
+
